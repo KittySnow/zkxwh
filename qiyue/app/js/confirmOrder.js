f44596cdf480c57ef527b374e3f509644a6b0848
kitty <158086780@qq.com>
2017-05-17 23:01:06 +0800

    upload

--- qiyue/app/js/confirmOrder.js
+++ qiyue/app/js/confirmOrder.js
@@ -3,7 +3,7 @@
 (function (app) {
 
     app.controller( 'confirmOrder', ['$routeParams', '$timeout', '$http','$scope','$rootScope','tips', function ($routeParams, $timeout, $http,$scope,$rootScope,tips) {
-        $rootScope.hideBottom = 1;
+
     }]);
 
 })(angular.module('confirmOrder', []));
